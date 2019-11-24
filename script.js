871287b1cb8ee6f027bfab56027cf96d3abff68b
Justinas Slusnys <js1961@live.mdx.ac.uk>
2019-11-24 23:33:52 +0000

    registration and login

--- script.js
+++ script.js
@@ -1,37 +1,123 @@
-Vue.component('todo-item', {
-  props: ['task'],
-  template: '<li>{{ task.text }}</li>'
-})
-
-
-var app = new Vue({
-  el:'#app',
-  data: {
-    textbox: "",
-    todos: []
-  },
-  methods: {
-    addTodo: function() {
-      app.todos.push({text: this.textbox})
+Vue.component('log-in', {
+  data: function() {
+    return {
+      username: "",
+      password: "",
+      users: []
     }
+  },
+  template: `<transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+            <form>
+              <input v-model='username' required>
+              <input type="password" v-model='password' required>
+              <button v-on:click="logIn">LOG IN</button>
+            </form>
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+              <button class="modal-default-button" @click="$emit('close')">
+                OK
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>`,
+
+  methods: {
+
+    logIn: function(){
+      this.getUsers();
+      for(let user = 0; user < this.users.length; user++) {
+        if(this.users[user].username == this.username &&
+           this.users[user].password == this.password) {
+             this.users[user].isSignedIn = true;
+             localStorage.setItem("users", JSON.stringify(this.users));
+             break;
+        }
+      }
+    },
+
+    getUsers: function() {
+      if(localStorage.getItem("users") == null) {
+        this.users = [];
+
+      } else {
+        this.users = JSON.parse(localStorage.getItem("users"));
+      }
+    }
+
   }
 })
 
 
-var registration = new Vue({
-  el:'#reg',
-  data: {
-    userType: "",
-    username: "",
-    password: "",
-    users: []
+// register modal component
+Vue.component('sign-up', {
+  data: function() {
+    return {
+      usertype: "",
+      username: "",
+      password: "",
+      users: []
+    }
   },
+  template: `<transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+            <form>
+              <input type="radio" name="type" value="user" v-model="usertype" required> User <br>
+              <input type="radio" name="type" value="creator" v-model="usertype" required> Creator <br>
+              <input v-model='username' required>
+              <input type="password" v-model='password' required>
+              <button v-on:click='addUser'>SIGN UP</button>
+            </form>
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+              <button class="modal-default-button" @click="$emit('close')">
+                OK
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>`,
 
   methods: {
 
     addUser: function() {
       this.getUsers();
-      if(this.username == '' || this.password == '' || this.userType == '') {
+      if(this.username == '' || this.password == '' || this.usertype == '') {
         //do nothing
 
       } else if(this.invalidDetails()){
@@ -42,13 +128,14 @@ var registration = new Vue({
 
       } else {
           this.users.push({
-            userType: this.userType,
+            usertype: this.usertype,
             username: this.username,
             password: this.password,
-            isSignedIn: "false",
+            isSignedIn: false,
           });
           localStorage.setItem("users", JSON.stringify(this.users));
           alert("You Have Successfully Registered");
+          $emit('close');
 
       }
     },
@@ -82,4 +169,57 @@ var registration = new Vue({
       }
     }
   }
+})
+
+// start app
+const app = new Vue({
+  el: '#app',
+  data: {
+    showRegModal: false,
+    showLogModal: false,
+    showAddCourseModal: false,
+    isLogedIn: false,
+    logedInUser: '',
+    users: []
+  },
+  methods: {
+
+    loginStatus: function() {
+      this.getUsers();
+      for(let user = 0; user < this.users.length; user++) {
+        if(this.users[user].isSignedIn) {
+          console.log('got it');
+          this.isLogedIn = true;
+          this.logedInUser = this.users[user].username;
+          return true;
+          break;
+        } else {
+          this.isLogedIn = false;
+          this.logedInUser = '';
+          return false;
+        }
+      }
+    },
+
+    getUsers: function() {
+      if(localStorage.getItem("users") == null) {
+        this.users = [];
+
+      } else {
+        this.users = JSON.parse(localStorage.getItem("users"));
+      }
+    },
+
+    logout: function() {
+      for(let user = 0; user < this.users.length; user++) {
+        this.users[user].isSignedIn = false;
+        localStorage.setItem("users", JSON.stringify(this.users));
+      }
+      this.isLogedIn = false;
+    }
+  },
+
+  beforeMount(){
+    this.loginStatus();
+ }
 })
